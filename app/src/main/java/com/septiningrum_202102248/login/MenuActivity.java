56dd83cf5b6db3fd5e0700f9fd284383be6ad0b5
septiningrumputri <xeotietie@gmail.com>
2023-06-26 10:38:29 +0700

    Initial commit

---
+++ app/src/main/java/com/septiningrum_202102248/login/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.septiningrum_202102248.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MenuActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
